a18ebe6ccbc47748d0df468a3dd4d188efd78c3d
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-04-16 22:40:11 +0600

    add service details

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail />}></Route>
+
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
       <Footer />
 
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css'
 
 const Service = ({ service }) => {
     const { id, name, img, description, price } = service;
+
+    const navigate = useNavigate();
+    const navigateToServiceDetail = (id) => {
+        navigate(`/service/${id}`);
+    }
     return (
         <div className='container'>
             <div className='service'>
@@ -10,7 +16,7 @@ const Service = ({ service }) => {
                 <h2>{name}</h2>
                 <p>Price: {price}</p>
                 <p> <small>{description}</small></p>
-                <button className='btn btn-primary'>{name}</button>
+                <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary'>{name}</button>
             </div>
         </div>
     );
