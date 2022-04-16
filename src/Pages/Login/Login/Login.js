846e256e94850b277dbf09673460dd023694c0dc
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-04-16 23:47:52 +0600

    add logincomponent, toast, and re direct login component

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,31 +1,83 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
+import { useSignInWithEmailAndPassword, useUpdatePassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 
 const Login = () => {
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const [updatePassword, updating] = useUpdatePassword(auth);
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message} </p>
+        toast(errorElement)
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password);
+    }
+
+    const navigateRegister = () => {
+        navigate('/register');
+
+    }
+
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+
+        if (email) {
+            updatePassword(email);
+            toast("Email Sent");
+        } else {
+            toast("Enter Your Email");
+        }
+    }
+
+
+
     return (
         <div className='container w-50 mx-auto '>
             <h2 className='text-primary text-center mt-2'>Please Login</h2>
-            <Form >
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Button className='mx-auto w-100 rounded-pill' variant="primary" type="submit">
                     Login
                 </Button>
-                {/* {errorElement} */}
-                <p>New to Modern Dental? <Link to={'/register'} className='test-danger text-decoration-none' > Please Register</Link ></p>
-                <p>Forget Password? <button className='btn btn-link text-primary text-decoration-none ' > Reset Password </button ></p>
+                <p>New to Modern Dental? <Link to={'/register'} className='test-danger text-decoration-none' onClick={navigateRegister}> Please Register</Link ></p>
+                <p>Forget Password? <button className='btn btn-link text-primary text-decoration-none ' onClick={resetPassword}> Reset Password </button ></p>
             </Form>
             <SocialLogin></SocialLogin>
             <ToastContainer />
